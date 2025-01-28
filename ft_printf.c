7e817c682fe96b3d46d40bc880c6077e5480c0d5
Super_E <evertvillca@gmail.com>
2025-01-28 02:04:20 +0100

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,65 @@
 #include "ft_printf.h"
-#include <stdio.h>
+#include <stdio.h> // eliminar
+
+static void handle_char(char const *format, va_list argptr, int *count)
+{
+	int	aux;
+	char *str;
+
+	if (*format == 'c')
+	{
+		aux = va_arg(argptr, int);
+		*count += write(1, &aux, 1);
+	}
+	else if (*format == 's')
+	{
+		str = va_arg(argptr, char *);
+		ft_putstr(str, count);
+	}
+
+}
+
+static void handle_pointer(char const *format, va_list argptr, int *count)
+{
+	unsigned long long addr;
+
+	if (*format == 'p')
+	{
+		addr = va_arg(argptr, unsigned long long);
+		if (addr == 0)
+		{
+			*count += write(1, "(nil)", 5);
+			return ;
+		}
+		*count += write(1, "0x", 2);
+		print_base(addr, "0123456789abcdef", 16, count);
+	}
+}
+
+static void handle_numbers(char const *format, va_list argptr, int *count)
+{
+	if (*format == 'd' || *format == 'i')
+		ft_putnbr(va_arg(argptr, int), count);
+	else if (*format == 'u')
+		print_base(va_arg(argptr, unsigned int), "0123456789", 10, count);
+	else if (*format == 'x')
+		print_base(va_arg(argptr, unsigned int), "0123456789abcdef", 16, count);
+	else if (*format == 'X')
+		print_base(va_arg(argptr, unsigned int), "0123456789ABCDEF", 16, count);
+}
+
+static void	parse_format(char const *format, va_list argptr, int *count)
+{
+	if (*format == 'c' || *format == 's')
+		handle_char(format, argptr, count);
+	else if (*format == 'p')
+		handle_pointer(format, argptr, count);
+	else if (*format == 'd' || *format == 'i' || *format == 'u'
+			|| *format == 'x' || *format == 'X')
+		handle_numbers(format, argptr, count);
+	else if (*format == '%')
+		*count += write(1, "%", 1);
+}
 
 int		ft_printf(char const *format, ...)
 {
@@ -22,11 +82,6 @@ int		ft_printf(char const *format, ...)
 	}
 	va_end(argptr);
 	return (count);
-}
-
-static parse_format(char const *format, va_list argptr, int *count)
-{
-	
 }
 
 /*
--- ft_printf.h
+++ ft_printf.h
@@ -6,5 +6,8 @@
 #include <unistd.h>
 
 int		ft_printf(char const *format, ...);
+void	ft_putstr(char *str, int *count);
+void	ft_putnbr(int nbr, int *count);
+void	print_base(unsigned long long n, char *base, int base_len, int *count);
 
 #endif
